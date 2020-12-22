9354d6b9a34f9fa2e6fc13641bf67b61bfa62917
emilypry <emilypry@yahoo.com>
2020-12-22 14:03:36 -0600

    made structure of program

---
+++ Simple-web-app-with-classes/src/main/java/com/emily/Simple/web/app/with/classes/SimpleWebAppWithClassesApplication.java
@@ -0,0 +1,13 @@
+package com.emily.Simple.web.app.with.classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppWithClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebAppWithClassesApplication.class, args);
+	}
+
+}
