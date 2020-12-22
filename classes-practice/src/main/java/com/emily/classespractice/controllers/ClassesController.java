c9b80a99719be16b4a813051331fde8875ad0008
emilypry <emilypry@yahoo.com>
2020-12-22 14:30:04 -0600

    app is working

---
+++ classes-practice/src/main/java/com/emily/classespractice/controllers/ClassesController.java
@@ -0,0 +1,15 @@
+package com.emily.classespractice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ClassesController{
+    @GetMapping("")
+    public String index(){
+        return "index";
+    }
+
+}
