798345304479fbab2e4dd67d353edf5054bf9e04
emilypry <emilypry@yahoo.com>
2020-12-22 18:20:41 -0600

    started /mybudget

--- classes-practice/src/main/java/com/emily/classespractice/controllers/ClassesController.java
+++ classes-practice/src/main/java/com/emily/classespractice/controllers/ClassesController.java
@@ -3,21 +3,32 @@ package com.emily.classespractice.controllers;
 import com.emily.classespractice.models.Budget;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ClassesController{
+    private Budget myBudget;
+
     @GetMapping("")
     public String index(){
         return "index";
     }
 
     @PostMapping("")
-    @ResponseBody
     public String makeNewBudget(@ModelAttribute Budget budget){
-        return "Added budget with "+budget.getTotalAmount();
+        myBudget = budget;
+        return "redirect:mybudget";
+    }
+
+    @GetMapping("mybudget")
+    public String showBudget(Model model){
+        model.addAttribute("amount", myBudget.getTotalAmount());
+        return "viewbudget";
     }
 }
